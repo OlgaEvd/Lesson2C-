f8d136dd5915d198a2a33ec982852533ee35738a
«Olga» <elfica@yandex.ru>
2022-06-24 23:22:41 +0800

    Программа, которая выдаёт вторую цифру трехзначного числа

---
+++ HW_Ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Программа, которая выдаёт вторую цифру трехзначного числа");
+Console.WriteLine("");
+Console.Write("Введите трехзначное число : ");
+
+void SnowNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    string result = Convert.ToString(num);
+    Console.WriteLine("Вторая цифра этого числа -> " + result[1]);
+}
+SnowNumber();
