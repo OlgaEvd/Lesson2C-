0a3992f89b61692ff08810ee3448aacc0d6138ad
«Olga» <elfica@yandex.ru>
2022-06-24 16:40:50 +0800

    Example11

---
+++ Ex11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+void  SnowNumber()
+
+{
+int num = new Random().Next (100,1000);
+int num1 = num/100;
+int num2 = num % 10;
+int result = num1 * 10 + num2;
+Console.WriteLine(num + " ->" + result);
+}
+SnowNumber();
