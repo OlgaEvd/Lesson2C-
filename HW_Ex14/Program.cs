41bb5ef58e0c5d0cd486986290c4bfbdf7095200
«Olga» <elfica@yandex.ru>
2022-06-25 00:13:58 +0800

    Кратно ли число 7 и 23 одновременно

---
+++ HW_Ex14/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 14: Напишите программу, которая принимает на
+//вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Кратно ли число 7 и 23 одновременно");
+Console.WriteLine("");
+Console.Write("Введите число : ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 7;
+int b = 23;
+
+if (number % a == 0 && number % b == 0)
+{
+Console.WriteLine("Кратно обоим числам");
+}
+
+else if (number % a == 0) 
+{
+    Console.WriteLine($"кратно только числу {a}");
+}
+
+else if (number % b == 0) 
+{
+    Console.WriteLine($"кратно только числу {b}");
+}
+else
+{
+    Console.WriteLine(" Не кратно ни одному числу");
+}
